40004eba52e58898e5d7db6a3a96e66ca9d4884e
melpd <melanie.desroches@uconn.edu>
2024-04-09 11:48:30 -0400

    imported the sheets from excel

--- Foster_and_Adoption_Analysis.py
+++ Foster_and_Adoption_Analysis.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
-states_df= pd.read_excel("C:/Users/mpdes/Downloads/state-afcars-data-2013-2022.xlsx")
-head(states_df)
+xls= pd.ExcelFile("C:/Users/mpdes/Downloads/state-afcars-data-2013-2022.xlsx")
+# Read specific sheets
+served_df = pd.read_excel(xls, 'Served')
+in_care_df = pd.read_excel(xls, 'In Care on September 30th')
+entered_df = pd.read_excel(xls, 'Entered')
+exited_df = pd.read_excel(xls, 'Exited')
+waiting_df = pd.read_excel(xls, 'Waiting for Adoption')
+terminated_df = pd.read_excel(xls, 'Parental Rights Terminated')
+adopted_df = pd.read_excel(xls, 'Adopted')
+print(adopted_df.head(20))
