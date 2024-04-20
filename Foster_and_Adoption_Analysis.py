81a24021074801d9d45a7d2cebcee1fa5c126253
melpd <melanie.desroches@uconn.edu>
2024-04-20 17:11:25 -0400

    actually cleaned the data

--- Foster_and_Adoption_Analysis.py
+++ Foster_and_Adoption_Analysis.py
@@ -10,15 +10,18 @@ waiting_df = pd.read_excel(xls, 'Waiting for Adoption')
 terminated_df = pd.read_excel(xls, 'Parental Rights Terminated')
 adopted_df = pd.read_excel(xls, 'Adopted')
 
-#TO DO:
 #- Data Cleaning
-#   -Remove top section with titles
-# remove the first 5 rows
-# Remove top section with titles
+
+# Clean and format dataframes
 def clean_dataframe(df):
-    df = df.iloc[6:,].reset_index(drop=True)
-    # Rename the first column to 'States'
-    df = df.rename(columns={df.columns[0]: 'States'})
+    df = df.iloc[7:].reset_index(drop=True) # Remove top section with titles
+    df.columns.values[0] = 'States' # Rename the first column to 'States'
+    years = range(2013, 2023)
+    df.columns = ['States'] + list(years) # Set columns as years 2013-2022
+    df.set_index('States', inplace=True)
+    df.fillna(0, inplace=True)
+    for col in df.columns:
+        df[col] = df[col].astype(int)
     return df
 # Clean dataframes by removing empty spaces
 served_df = clean_dataframe(served_df)
@@ -28,31 +31,13 @@ waiting_df = clean_dataframe(waiting_df)
 terminated_df = clean_dataframe(terminated_df)
 adopted_df = clean_dataframe(adopted_df)
 
-#set the columns for each data frame
-def set_years(df):
-    years = range(2013, 2023)
-    df.columns = ['States'] + list(years)
-# Set columns as years for all dataframes
-set_years(served_df)
-set_years(in_care_df)
-set_years(entered_df)
-set_years(waiting_df)
-set_years(terminated_df)
-set_years(adopted_df)
-
-# Reformat first Row
-def reformat_row(df):
-    for i in range(1,11):
-        year_str = df.iloc[0, i]  # Accessing the first column
-        year = int(year_str.split()[-1])  # Extracting the year and converting to integer
-        df.iloc[0, i] = year
-reformat_row(served_df)
-reformat_row(in_care_df)
-reformat_row(entered_df)
-reformat_row(waiting_df)
-reformat_row(terminated_df)
-reformat_row(adopted_df)
-print(served_df.head(5))
+served_df = served_df.drop(served_df.index[-1])
+in_care_df = in_care_df.drop(in_care_df.index[-1])
+entered_df = entered_df.drop(entered_df.index[-1])
+waiting_df = waiting_df.drop(waiting_df.index[-1])
+terminated_df = terminated_df.drop(terminated_df[-1])
+adopted_df = adopted_df.drop(adopted_df.index[-1])
 print(served_df.columns)
-
-print(served_df.dtpyes)
+print(served_df.dtypes)
+print(served_df.head(5))
+print(served_df.tail(10))
