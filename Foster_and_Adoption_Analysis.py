3de9c9b4f827ca8c825d2141ca155092c3382e17
melpd <melanie.desroches@uconn.edu>
2024-04-20 16:26:07 -0400

    slight changes

--- Foster_and_Adoption_Analysis.py
+++ Foster_and_Adoption_Analysis.py
@@ -9,7 +9,6 @@ exited_df = pd.read_excel(xls, 'Exited')
 waiting_df = pd.read_excel(xls, 'Waiting for Adoption')
 terminated_df = pd.read_excel(xls, 'Parental Rights Terminated')
 adopted_df = pd.read_excel(xls, 'Adopted')
-#print(adopted_df.head(20))
 
 #TO DO:
 #- Data Cleaning
@@ -21,40 +20,26 @@ def clean_dataframe(df):
     # Rename the first column to 'States'
     df = df.rename(columns={df.columns[0]: 'States'})
     return df
-# Clean dataframes
+# Clean dataframes by removing empty spaces
 served_df = clean_dataframe(served_df)
-#in_care_df = clean_dataframe(in_care_df)
-#entered_df = clean_dataframe(entered_df)
-#waiting_df = clean_dataframe(waiting_df)
-#terminated_df = clean_dataframe(terminated_df)
-#adopted_df = clean_dataframe(adopted_df)
+in_care_df = clean_dataframe(in_care_df)
+entered_df = clean_dataframe(entered_df)
+waiting_df = clean_dataframe(waiting_df)
+terminated_df = clean_dataframe(terminated_df)
+adopted_df = clean_dataframe(adopted_df)
 
+#set the columns for each data frame
 def set_years(df):
     years = range(2013, 2023)
     df.columns = ['States'] + list(years)
 # Set columns as years for all dataframes
 set_years(served_df)
-#set_years(in_care_df)
-#set_years(entered_df)
-#set_years(waiting_df)
-#set_years(terminated_df)
-#set_years(adopted_df)
+set_years(in_care_df)
+set_years(entered_df)
+set_years(waiting_df)
+set_years(terminated_df)
+set_years(adopted_df)
 
-print(served_df.head(5))
-print(served_df.columns)
-"""served_df = served_df.iloc[6:,]
-served_df.reset_index(drop=True, inplace=True)
-in_care_df = in_care_df.iloc[6:,]
-in_care_df.reset_index(drop=True, inplace=True)
-entered_df = entered_df.iloc[6:,]
-entered_df.reset_index(drop=True, inplace=True)
-waiting_df = waiting_df.iloc[6:,]
-waiting_df.reset_index(drop=True, inplace=True)
-terminated_df = terminated_df.iloc[6:,]
-terminated_df.reset_index(drop=True, inplace=True)
-adopted_df = adopted_df.iloc[6:,]
-adopted_df.reset_index(drop=True, inplace=True)
-#print(served_df.head(20))"""
 # Reformat first Row
 def reformat_row(df):
     for i in range(1,11):
@@ -62,10 +47,10 @@ def reformat_row(df):
         year = int(year_str.split()[-1])  # Extracting the year and converting to integer
         df.iloc[0, i] = year
 reformat_row(served_df)
-#reformat_row(in_care_df)
-#reformat_row(entered_df)
-#reformat_row(waiting_df)
-#reformat_row(terminated_df)
-#reformat_row(adopted_df)
+reformat_row(in_care_df)
+reformat_row(entered_df)
+reformat_row(waiting_df)
+reformat_row(terminated_df)
+reformat_row(adopted_df)
 print(served_df.head(5))
 print(served_df.columns)
