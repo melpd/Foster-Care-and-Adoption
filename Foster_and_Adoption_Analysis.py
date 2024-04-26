18388ba293fdf1c49eddcf472c2f7ead2b383f35
melpd <melanie.desroches@uconn.edu>
2024-04-25 20:09:00 -0400

    Added Predictions Visualizations

--- Foster_and_Adoption_Analysis.py
+++ Foster_and_Adoption_Analysis.py
@@ -45,8 +45,6 @@ exited_df = exited_df.iloc[:-1]
 served_df_excluding_pr_and_total = served_df[~served_df.index.str.contains('Puerto Rico|Total')]
 total_served_per_year = served_df_excluding_pr_and_total.sum(axis=0)
 total_served_per_year_df = total_served_per_year.to_frame().T
-print("Total Served per Year (Excluding Puerto Rico and Total):")
-print(total_served_per_year_df)
 
 in_care_df_excluding_pr_and_total = in_care_df[~in_care_df.index.str.contains('Puerto Rico|Total')]
 total_in_care_per_year = in_care_df_excluding_pr_and_total.sum(axis=0)
@@ -76,7 +74,8 @@ total_adopted_per_year_df = total_adopted_per_year.to_frame().T
 exited_df_excluding_pr_and_total = exited_df[~exited_df.index.str.contains('Puerto Rico|Total')]
 total_exited_per_year = exited_df_excluding_pr_and_total.sum(axis=0)
 total_exited_per_year_df = total_exited_per_year.to_frame().T
-print(total_adopted_per_year_df)
+print(total_exited_per_year_df)
+
 import matplotlib.pyplot as plt
 """
 # Plot the total number of children served over the years
@@ -152,7 +151,7 @@ plt.title('Total Number of Children Exiting Foster Care per Year (Excluding Puer
 plt.xlabel('Year')
 plt.ylabel('Total Number of Children Exiting Foster Care')
 plt.grid(True)
-plt.xticks(total_exiting_per_year_df.columns.astype(int), rotation=45)
+plt.xticks(total_exited_per_year_df.columns.astype(int), rotation=45)
 plt.tight_layout()
 plt.show()
 
@@ -201,7 +200,7 @@ plt.show()
 usa_in_care = usa_states.merge(in_care_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
 fig, ax = plt.subplots(1, 1, figsize=(42, 30))
 usa_adoption.plot(column=2022, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
-plt.title('Number of In the Foster Care System in the United States (2022)')
+plt.title('Number of Children In-Care in the United States (2022)')
 plt.axis('off')
 plt.show()
 
@@ -321,10 +320,12 @@ for state, data in adopted_df_excluding_pr_and_total.iterrows():
     model = ARIMA(train_data, order=(1, 0, 1))
     fit_model = model.fit()
     # Make forecasts
-    forecast = fit_model.forecast(steps=2)
+    forecast = fit_model.forecast(steps=3)
     #print(state, forecast)
     adopted_df_excluding_pr_and_total.loc[state, 2023] = forecast.iloc[0]
     adopted_df_excluding_pr_and_total.loc[state, 2024] = forecast.iloc[1]
+    adopted_df_excluding_pr_and_total.loc[state, 2025] = forecast.iloc[2]
+
 #print(adopted_df_excluding_pr_and_total.head())
 
 for state, data in in_care_df_excluding_pr_and_total.iterrows():
@@ -337,10 +338,11 @@ for state, data in in_care_df_excluding_pr_and_total.iterrows():
     model = ARIMA(train_data, order=(1, 0, 1))
     fit_model = model.fit()
     # Make forecasts
-    forecast = fit_model.forecast(steps=2)
+    forecast = fit_model.forecast(steps=3)
     #print(state, forecast)
     in_care_df_excluding_pr_and_total.loc[state, 2023] = forecast.iloc[0]
     in_care_df_excluding_pr_and_total.loc[state, 2024] = forecast.iloc[1]
+    in_care_df_excluding_pr_and_total.loc[state, 2025] = forecast.iloc[2]
 #print(in_care_df_excluding_pr_and_total.head())
 
 for state, data in entered_df_excluding_pr_and_total.iterrows():
@@ -353,10 +355,11 @@ for state, data in entered_df_excluding_pr_and_total.iterrows():
     model = ARIMA(train_data, order=(1, 0, 1))
     fit_model = model.fit()
     # Make forecasts
-    forecast = fit_model.forecast(steps=2)
+    forecast = fit_model.forecast(steps=3)
     #print(state, forecast)
     entered_df_excluding_pr_and_total.loc[state, 2023] = forecast.iloc[0]
     entered_df_excluding_pr_and_total.loc[state, 2024] = forecast.iloc[1]
+    entered_df_excluding_pr_and_total.loc[state, 2025] = forecast.iloc[2]
 #print(entered_df_excluding_pr_and_total.head())
 
 for state, data in waiting_df_excluding_pr_and_total.iterrows():
@@ -369,10 +372,11 @@ for state, data in waiting_df_excluding_pr_and_total.iterrows():
     model = ARIMA(train_data, order=(1, 0, 1))
     fit_model = model.fit()
     # Make forecasts
-    forecast = fit_model.forecast(steps=2)
+    forecast = fit_model.forecast(steps=3)
     #print(state, forecast)
     waiting_df_excluding_pr_and_total.loc[state, 2023] = forecast.iloc[0]
     waiting_df_excluding_pr_and_total.loc[state, 2024] = forecast.iloc[1]
+    waiting_df_excluding_pr_and_total.loc[state, 2025] = forecast.iloc[2]
 #print(waiting_df_excluding_pr_and_total.head())
 
 for state, data in exited_df_excluding_pr_and_total.iterrows():
@@ -385,11 +389,12 @@ for state, data in exited_df_excluding_pr_and_total.iterrows():
     model = ARIMA(train_data, order=(1, 0, 1))
     fit_model = model.fit()
     # Make forecasts
-    forecast = fit_model.forecast(steps=2)
+    forecast = fit_model.forecast(steps=3)
     #print(state, forecast)
     exited_df_excluding_pr_and_total.loc[state, 2023] = forecast.iloc[0]
     exited_df_excluding_pr_and_total.loc[state, 2024] = forecast.iloc[1]
-print(exited_df_excluding_pr_and_total.head())
+    exited_df_excluding_pr_and_total.loc[state, 2025] = forecast.iloc[2]
+
 
 for state, data in terminated_df_excluding_pr_and_total.iterrows():
     train_data = data.dropna()
@@ -401,10 +406,11 @@ for state, data in terminated_df_excluding_pr_and_total.iterrows():
     model = ARIMA(train_data, order=(1, 0, 1))
     fit_model = model.fit()
     # Make forecasts
-    forecast = fit_model.forecast(steps=2)
+    forecast = fit_model.forecast(steps=3)
     #print(state, forecast)
     terminated_df_excluding_pr_and_total.loc[state, 2023] = forecast.iloc[0]
     terminated_df_excluding_pr_and_total.loc[state, 2024] = forecast.iloc[1]
+    terminated_df_excluding_pr_and_total.loc[state, 2025] = forecast.iloc[2]
 #print(terminated_df_excluding_pr_and_total.head())
 
 for state, data in served_df_excluding_pr_and_total.iterrows():
@@ -417,8 +423,178 @@ for state, data in served_df_excluding_pr_and_total.iterrows():
     model = ARIMA(train_data, order=(1, 0, 1))
     fit_model = model.fit()
     # Make forecasts
-    forecast = fit_model.forecast(steps=2)
+    forecast = fit_model.forecast(steps=3)
     #print(state, forecast)
     served_df_excluding_pr_and_total.loc[state, 2023] = forecast.iloc[0]
     served_df_excluding_pr_and_total.loc[state, 2024] = forecast.iloc[1]
+    served_df_excluding_pr_and_total.loc[state, 2025] = forecast.iloc[2]
 print(served_df_excluding_pr_and_total.head())
+
+#Heatmaps of Updated Predictions
+# Plot the total number of children served over the years
+total_served_per_year = served_df_excluding_pr_and_total.sum(axis=0)
+total_served_per_year_df = total_served_per_year.to_frame().T
+plt.figure(figsize=(10, 6))
+plt.plot(total_served_per_year_df.columns.astype(int), total_served_per_year_df.values.flatten(), marker='o', linestyle='-')
+plt.title('Total Number of Children in Foster Care per Year (Excluding Puerto Rico)')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Children Served by the Foster Care System')
+plt.grid(True)
+plt.xticks(total_served_per_year_df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+#Plot the number of children in-care at the end of each year
+total_in_care_per_year = in_care_df_excluding_pr_and_total.sum(axis=0)
+total_in_care_per_year_df = total_in_care_per_year.to_frame().T
+plt.figure(figsize=(10, 6))
+plt.plot(total_in_care_per_year_df.columns.astype(int), total_in_care_per_year_df.values.flatten(), marker='o', linestyle='-')
+plt.title('Total Number of Children In-Care per Year (Excluding Puerto Rico)')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Children Still In Care at the end of the Year')
+plt.grid(True)
+plt.xticks(total_in_care_per_year_df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+#Plot of the number of children that are waiting to be adopted per year
+total_waiting_per_year = waiting_df_excluding_pr_and_total.sum(axis=0)
+total_waiting_per_year_df = total_waiting_per_year.to_frame().T
+plt.figure(figsize=(10, 6))
+plt.plot(total_waiting_per_year_df.columns.astype(int), total_waiting_per_year_df.values.flatten(), marker='o', linestyle='-')
+plt.title('Total Number of Children that are Waiting to be Adopted Each Year (Excluding Puerto Rico)')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Children Waiting to be Adopted')
+plt.grid(True)
+plt.xticks(total_waiting_per_year_df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+#Plot of the number of children that entered the foster care system per year
+total_entered_per_year = entered_df_excluding_pr_and_total.sum(axis=0)
+total_entered_per_year_df = total_entered_per_year.to_frame().T
+plt.figure(figsize=(10, 6))
+plt.plot(total_entered_per_year_df.columns.astype(int), total_entered_per_year_df.values.flatten(), marker='o', linestyle='-')
+plt.title('Total Number of Children that Entered the Foster Care System Each Year (Excluding Puerto Rico)')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Children Entering Foster Care')
+plt.grid(True)
+plt.xticks(total_entered_per_year_df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+#Plot of the number of parental rights terminated per year
+total_terminated_per_year = terminated_df_excluding_pr_and_total.sum(axis=0)
+total_terminated_per_year_df = total_terminated_per_year.to_frame().T
+plt.figure(figsize=(10, 6))
+plt.plot(total_terminated_per_year_df.columns.astype(int), total_terminated_per_year_df.values.flatten(), marker='o', linestyle='-')
+plt.title('Total Number of Parental Rights Terminated Each Year (Excluding Puerto Rico)')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Parental Rights Terminated')
+plt.grid(True)
+plt.xticks(total_terminated_per_year_df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+# Plot the total number of children adopted over the years
+total_adopted_per_year = adopted_df_excluding_pr_and_total.sum(axis=0)
+total_adopted_per_year_df = total_adopted_per_year.to_frame().T
+plt.figure(figsize=(10, 6))
+plt.plot(total_adopted_per_year_df.columns.astype(int), total_adopted_per_year_df.values.flatten(), marker='o', linestyle='-')
+plt.title('Total Number of Children Adopted per Year (Excluding Puerto Rico)')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Children Adopted per Year')
+plt.grid(True)
+plt.xticks(total_adopted_per_year_df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+# Plot the total number of children exiting over the years
+total_exited_per_year = exited_df_excluding_pr_and_total.sum(axis=0)
+total_exited_per_year_df = total_exited_per_year.to_frame().T
+plt.figure(figsize=(10, 6))
+plt.plot(total_exited_per_year_df.columns.astype(int), total_exited_per_year_df.values.flatten(), marker='o', linestyle='-')
+plt.title('Total Number of Children Exiting Foster Care per Year (Excluding Puerto Rico)')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Children Exiting Foster Care')
+plt.grid(True)
+plt.xticks(total_exited_per_year_df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+total_per_year_list = [total_exited_per_year_df ,total_entered_per_year_df, total_adopted_per_year_df, total_in_care_per_year_df, total_terminated_per_year_df, total_served_per_year_df, total_waiting_per_year_df]
+labels = ['Exited','Entered', 'Adopted', 'In-Care', 'Terminated', 'Served', 'Waiting']
+combined_df = pd.concat(total_per_year_list)
+# Plot the total number of children served per year for each dataframe
+plt.figure(figsize=(10, 6))
+for df, label in zip(total_per_year_list, labels):
+    plt.plot(df.columns.astype(int), df.values.flatten(), marker='o', linestyle='-', label=label)
+plt.title('Total Number of Children Served per Year')
+plt.xlabel('Year')
+plt.ylabel('Total Number of Children Served')
+plt.grid(True)
+plt.legend()
+plt.xticks(df.columns.astype(int), rotation=45)
+plt.tight_layout()
+plt.show()
+
+#Heatmap
+import geopandas as gpd
+
+# Load the shapefile of the United States
+usa = gpd.read_file("C:/Users/mpdes/Downloads/cb_2022_us_all_500k/cb_2022_us_state_500k/cb_2022_us_state_500k.shp")
+
+# Filter out US territories
+usa_states = usa[~usa['NAME'].isin(['United States Virgin Islands', 'Guam', 'Puerto Rico', 'Commonwealth of the Northern Mariana Islands', 'American Samoa'])]
+
+# Merge the filtered shapefile with adoption data
+usa_adoption = usa_states.merge(adopted_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
+
+# Plot the map using the number of children adopted in 2025
+fig, ax = plt.subplots(1, 1, figsize=(42, 30))
+usa_adoption.plot(column=2025, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Number of Children Adopted in the United States (2025)')
+plt.axis('off')
+plt.show()
+
+usa_served = usa_states.merge(served_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
+fig, ax = plt.subplots(1, 1, figsize=(42, 30))
+usa_served.plot(column=2025, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Number of Children Served by the Foster Care System in the United States (2025)')
+plt.axis('off')
+plt.show()
+
+usa_in_care = usa_states.merge(in_care_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
+fig, ax = plt.subplots(1, 1, figsize=(42, 30))
+usa_in_care.plot(column=2025, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Number of Children Currently In-Care in the United States (2025)')
+plt.axis('off')
+plt.show()
+
+usa_entered = usa_states.merge(entered_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
+fig, ax = plt.subplots(1, 1, figsize=(42, 30))
+usa_entered.plot(column=2025, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Number of Children that Entered the Foster Care System in the United States (2025)')
+plt.axis('off')
+plt.show()
+
+usa_waiting = usa_states.merge(waiting_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
+fig, ax = plt.subplots(1, 1, figsize=(42, 30))
+usa_adoption.plot(column=2025, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Number of Children Waiting to be Adopted in the United States (2025)')
+plt.axis('off')
+plt.show()
+
+usa_terminated = usa_states.merge(terminated_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
+fig, ax = plt.subplots(1, 1, figsize=(42, 30))
+usa_terminated.plot(column=2025, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Number of Children With Parental Rights Terminated in the United States (2025)')
+plt.axis('off')
+plt.show()
+
+usa_exited = usa_states.merge(exited_df_excluding_pr_and_total, how='left', left_on='NAME', right_index=True)
+fig, ax = plt.subplots(1, 1, figsize=(42, 30))
+usa_exited.plot(column=2025, cmap='YlGnBu', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Number of Children Exiting Foster Care in the United States (2025)')
+plt.axis('off')
+plt.show()
